1d433c5233a71d0ba2269c4cb63f8097f03c802c
Hatami Ra'is Bukhari <hatamierbe12@gmail.com>
2024-05-29 13:24:47 +0700

    complete ch 4, onto ch 5

--- lib/main.dart
+++ lib/main.dart
@@ -27,27 +27,55 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNextWord(){
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
         children: [
-          Text('A random AWESOME idea:'),
-          Text(appState.current.asLowerCase),
+          Text('A random word is:'),
+          BigCard(pair: pair),
 
           // ↓ Add this.
           ElevatedButton(
             onPressed: () {
-              print('button pressed!');
+              appState.getNextWord();
             },
-            child: Text('Next Word'),
+            child: Text('Generate random word'),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(color: theme.colorScheme.onPrimary);
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(pair.asLowerCase, style: style,),
       ),
     );
   }
